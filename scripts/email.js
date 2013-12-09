07838c0dd9802d74083b2b324e98b7a85b6c38af
Phong <pthieu@gmail.com>
2013-12-09 01:07:03 -0500

    fixing email.js tab spaces

--- scripts/email.js
+++ scripts/email.js
@@ -1,28 +1,41 @@
 var nodemailer = require("nodemailer");
 
 var smtpTransport =  nodemailer.createTransport("SMTP",{
-   service: "Gmail",
-   auth: {
-        user: "iokingpoll@gmail.com",
-        pass: "Poopoo123"
-   }
+     service: "Gmail",
+     auth: {
+                user: "iokingpoll@gmail.com",
+                pass: "Poopoo123"
+     }
 });
 
 function send_email(email, subject, msg){
     smtpTransport.sendMail({
-        from: "KingPoll <iokingpoll@gmail.com>",
-        to: email,
-        subject: subject,
-        text: msg
-    }, function(error,response){
-        if(error){
-	    console.log(error);
-        }else{
-	    console.log(response);
-	}
-	smtpTransport.close();
+            from: "KingPoll <iokingpoll@gmail.com>",
+            to: email,
+            subject: subject,
+            text: msg
+    }, function(err, res){
+            smtpTransport.close();
+            if(err){
+                console.log(err);
+                handle_sendemail_success(false);
+            }
+            else{
+                console.log(res);
+                handle_sendemail_success(true);
+            }
     });
+}
+
+function handle_sendemail_success(results)
+{
+    if (results){
+        //update db here
+    }
+    else if (!results){
+        //try to email again?
+    }
     
 }
 
-//send_email('iokingpoll@gmail.com', 'Test', 'testttt');
+// send_email('iokingpoll@gmail.com', 'Test', 'testttt');
