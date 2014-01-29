754bdd995a6b33e4e81df73e5756b0035d8f3a19
Adrian Chung <adrian.c.chung@gmail.com>
2014-01-28 22:01:31 -0500

    Add user_id to the comments model

--- models/comments.js
+++ models/comments.js
@@ -7,9 +7,10 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var commentsSchema = new mongoose.Schema({
     parent_poll_id    : { type: String, required: true },
     parent_comment_id : { type: ObjectId, required: false },
-    message         : { type: String, required: true },
-    comments        : [mongoose.Schema.Types.ObjectId],
-    created_date    : { type: Date, default: Date.now },
-    updated_date    : { type: Date, default: Date.now }
+    user_id           : { type: String },
+    message           : { type: String, required: true },
+    comments          : [mongoose.Schema.Types.ObjectId],
+    created_date      : { type: Date, default: Date.now },
+    updated_date      : { type: Date, default: Date.now }
 });
 exports.Comment = mongoose.model("comments", commentsSchema);
