5da2940a996150bec16301d8f6d40cdd3378ede9
Adrian Chung <adrian.c.chung@gmail.com>
2014-02-05 20:43:22 -0500

    Add ability to post anonymous comments

--- models/comments.js
+++ models/comments.js
@@ -5,11 +5,11 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var commentsSchema = new mongoose.Schema({
-    parent_poll_id    : { type: String, required: true },
+    parent_poll_id    : { type: String, required: true, index: true },
     parent_comment_id : { type: ObjectId, required: false },
     user_id           : { type: String },
     message           : { type: String, required: true },
-    comments          : [mongoose.Schema.Types.ObjectId],
+    comments          : [{ type: mongoose.Schema.Types.ObjectId, ref: this }],
     created_date      : { type: Date, default: Date.now },
     updated_date      : { type: Date, default: Date.now }
 });
