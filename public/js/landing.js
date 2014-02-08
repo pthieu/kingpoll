4c808573d0123b06144a1fb5e2115c773f3bbc23
Phong <pthieu@gmail.com>
2014-02-07 23:31:15 -0500

    poll list table aesthetic changes

--- public/js/landing.js
+++ public/js/landing.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
             var tmpdesc = (poll[i].p_desc) ? poll[i].p_desc : "Poll King did not provide a description.";
             var th_colspan = $('thead tr td').length;
             var _embeddata = poll[i].p_embed;
-            $('#polls-wrap').append('<td class="embed-wrap" colspan="'+(th_colspan+1)+'"><div class="polldesc" style="display: none" poll-id='+poll[i].p_id+" "+((_embeddata)?("embed-data=\""+_embeddata+"\""):"")+'>'+tmpdesc+'</div></td>');
+            $('#polls-wrap').append('<td class="desc-wrap" colspan="'+(th_colspan+1)+'"><div class="polldesc col-xs-10 col-xs-offset-1" style="display: none" poll-id='+poll[i].p_id+" "+((_embeddata)?("embed-data=\""+_embeddata+"\""):"")+'>'+tmpdesc+'</div></td>');
         }
         $('.polldesc').each(function () {
             $(this).html("<div>"+dual.linkify($(this).html()).text+"</div>")
