141ffc858a8e8f264520e0ca4d4fa09f28c189ce
Phong <pthieu@gmail.com>
2014-02-09 04:30:05 -0500

    adding custom checkboxes to verification page and aligning correctly

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,8 +1,8 @@
 var socket = io.connect();
+var tmp = colors_hex[randColor(colors_hex)];
 
 $(document).ready(function() {
     var code = (window.location.href).split('/')[4];
-    var tmp = colors_hex[randColor(colors_hex)];
     $('.btnSolo').css({'background-color': "#"+tmp,'border-color': "#"+tmp});
 
     var g_id = dual.getURLParameter('g');
@@ -17,10 +17,15 @@ $(document).ready(function() {
     	var v_date = $.now();
     	$('.validation-list').append(
     		'<tr>'+
-    		'<td>'+_data.poll.p_q+'</td>'+
+    		'<td class="right-align"><a href="/p/'+_data.poll.p_id+'" target="_blank">'+_data.poll.p_q+'</a></td>'+
     		'<td>'+_data.vote.v_text+'</td>'+
+    		'<td class="cb" data-valgroup="'+_data.vote.v_valid+'"><input type="radio" id="'+_data.poll.p_id+'-verify" class="radio-cb" name="'+_data.poll.p_id+'" checked/>'+
+            '<label for="'+_data.poll.p_id+'-verify" class="radio-cb-label"></label></td>"'+
+            '<td class="cb" data-valgroup="'+_data.vote.v_valid+'"><input type="radio" id="'+_data.poll.p_id+'-delete" class="radio-cb" name="'+_data.poll.p_id+'"/>'+
+            '<label for="'+_data.poll.p_id+'-delete" class="radio-cb-label"></label></td>"'+
     		'<td>'+v_date+'</td>'+
     		'</tr>'
 		);
+		$('.radio-cb-label').css({'color': "#"+tmp,'border-color': "#"+tmp});
     });
 });
