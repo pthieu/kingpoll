5e326af895262d3124618f33e3e0b41d8d5455ed
farran cheung <fcheung86@gmail.com>
2014-02-10 22:52:48 -0500

    Adding twitter login ability

--- routes/login_auth.js
+++ routes/login_auth.js
@@ -1,10 +1,14 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
 var User = require('../schema/userSchema').User;
 
 var FACEBOOK_APP_ID = "1447667995463132";
 var FACEBOOK_APP_SECRET = "2e522013d815555efb0db2673c4ff034";
+
+var TWITTER_APP_ID = "gTryKLMlN6NSQ2JkhRrA";
+var TWITTER_APP_SECRET = "aKa4RGbTevidRIalBPKNo6O4H0qV6O5p7MJ4OOtpvc";
 
 passport.serializeUser(function(user, done) {
   console.log("serialize authenticated...");
@@ -13,7 +17,7 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(user, done) {
   console.log("deserialize authenticated...");
-  if(user.u_thirdParty == "facebook") {
+  if(user.u_thirdParty == "facebook" || user.u_thirdParty == "twitter") {
     User.findOne({u_thirdId: user.u_thirdId}, function (err, fbuser) {
       if(err) { console.log("error"); }
       done(err, fbuser);
@@ -61,6 +65,39 @@ passport.use(new FacebookStrategy({
         newuser.u_name = profile.name.givenName + " " + profile.name.familyName;
         newuser.u_thirdId = profile.id;
         newuser.u_thirdParty = profile.provider;
+
+        newuser.save(function(err) {
+          if(err) { throw err; }
+          console.log('New user: ' + newuser.firstname + ' ' + newuser.lastname + ' created and logged in!');
+           return done(null, newuser);
+        });
+      }
+    });
+  })
+);
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_APP_ID,
+    consumerSecret: TWITTER_APP_SECRET,
+    callbackURL: "http://localhost:8888/auth/twitter/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log("twitter");
+    console.log(profile);
+    console.log(profile._json);
+    console.log(profile._json.id);
+    User.findOne({ 'u_thirdId': profile.id, 'u_thirdParty': 'twitter' }, function(err, user) {
+
+      if(user) {
+        console.log('User: ' + user.u_name + ' found and logged in!');
+        return done(null, user);
+      } else {
+        var newuser = new User();
+        newuser.u_id = profile._json.name;
+        newuser.u_email = profile._json.name + "@twitter.com";
+        newuser.u_name = profile._json.screen_name;
+        newuser.u_thirdId = profile._json.id;
+        newuser.u_thirdParty = "twitter";
 
         newuser.save(function(err) {
           if(err) { throw err; }
