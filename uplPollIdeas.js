ac8cfa3d16e3fb93d597a304c4def2a797fb51dc
Phong <pthieu@gmail.com>
2014-07-03 01:04:45 -0400

    adding in ideas for uplPolls js file

---
+++ uplPollIdeas.js
@@ -0,0 +1,21 @@
+var arrUPLPolls = [
+//do a check: if (typeof p_q == 'undefined'){ p_q = choices[0] vs. choices[1]}
+  {choices:['Creative', 'Logical'], colors:['red', 'blue']},
+  {choices:['Spontaneous', 'Analytical'], colors:['red', 'blue']}, // are these the same?
+  {choices:['Intuition', 'Preparation'], colors:['red', 'blue']}, // are these the same?
+  {choices:['Hardworking', 'Intelligent'], colors:['red', 'blue']},
+  {choices:['Cunning', 'Honorable'], colors:['red', 'blue']},
+  {choices:['Trusting', 'Skeptic'], colors:['red', 'blue']},
+  {choices:['Liberal', 'Conservative'], colors:['red', 'blue']},
+  {choices:['Youthful', 'Mature'], colors:['red', 'blue']},
+  {choices:['Optimist', 'Pessimist'], colors:['red', 'blue']},
+  {choices:['Extrovert', 'Introvert'], colors:['red', 'blue']},
+  {choices:['Brawn', 'Brain'], colors:['red', 'blue']},
+  {choices:['Solo', 'Team'], colors:['red', 'blue']},
+  {choices:['Fight', 'Flight'], colors:['red', 'blue']},
+  {choices:['Offense', 'Defense'], colors:['red', 'blue']},
+  {choices:['Strength', 'Speed'], colors:['red', 'blue']},
+  {p_q:'Fun?', choices:['Yes', 'No'], colors:['Green', 'Red']},
+  {p_q:'Attractive?', choices:['Yes', 'No'], colors:['Green', 'Red']},
+  {p_q:'Liar?', choices:['Yes', 'No'], colors:['Green', 'Red']},
+]
