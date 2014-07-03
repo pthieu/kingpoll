effae48e632992aaa4302b36f0bd69d21dde3908
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2014-07-02 22:50:30 -0400

    adding user to poll link schema

---
+++ schema/uplSchema.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+//schema to link polls to users for attr polls
+var uplSchema = new mongoose.Schema({
+	'u_id': {type:mongoose.Schema.Types.ObjectId, index:true, required:true},
+	'p_id': {type:mongoose.Schema.Types.ObjectId, index:true, required:true}
+});
+
+var UPL = mongoose.model('upl', uplSchema);
+module.exports.UPL = UPL;
