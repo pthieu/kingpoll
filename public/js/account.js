0be5f5c4d145ae41dedd2fde758bd1e33fe88bef
Phong <pthieu@gmail.com>
2014-07-25 02:03:12 -0400

    moving account legends down. enabling middle circle

--- public/js/account.js
+++ public/js/account.js
@@ -25,6 +25,7 @@ $(function () {
   });
 
   setTabs();
+  $('[data-index=attr-wrap]').click();
 });
 
 pie = (function (){
@@ -72,29 +73,31 @@ pie = (function (){
         .attr("width", w)
         .attr("height", h)
         .append("g")
-        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+        .attr("transform", "translate(" + w / 2 + "," + r2 + ")")
         .attr("class","upl_piechart");
 
-        dis.createLegend(r1);
+        dis.createLegend(r1,r2);
 
-        // var svg_pie_bg = svg.append("circle")
-        // .attr("cx", 0)
-        // .attr("cy", 0)
-        // .attr("r", innerRadius-10)
-        // .attr("fill", "none")
-        // .attr("id", "pieTotalBG");
-        // var svg_pie_msg = svg.append("text") // later svg is "higher"
-        //             .attr("class", "piechart_msg")
-        // svg_pie_msg.append("tspan")
-        //             .attr("x", 0)
-        //             .attr("y", -3)
-        //             .attr("id",'pie_msg_title')
-        //             .text("Total Votes:");
-        // svg_pie_msg.append("tspan")
-        //             .attr("x", 0)
-        //             .attr("y", 22)
-        //             .attr("id",'pie_msg_val')
-        //             .text("0");
+        var svg_pie_bg = svg.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", r1-10)
+        .attr("fill", "none")
+        .attr("class", "pieTotalBG");
+        var svg_pie_msg = svg.append("text") // later svg is "higher"
+                    .attr("class", "piechart_msg")
+        svg_pie_msg.append("tspan")
+                    .attr("x", 0)
+                    .attr("y", 1)
+                    .attr("class",'pie_msg_title')
+                    .attr("text-anchor", "middle")
+                    .text("Total Votes:");
+        svg_pie_msg.append("tspan")
+                    .attr("x", 0)
+                    .attr("y", 28)
+                    .attr("class",'pie_msg_val')
+                    .attr("text-anchor", "middle")
+                    .text("0");
       }
       var path = svg.selectAll("path")
       .data(pie_instance);
@@ -129,56 +132,66 @@ pie = (function (){
     });
   }
 
-  Donut.prototype.createLegend = function(r){
+  Donut.prototype.createLegend = function(r1, r2){
     var dis = this;
-    this.obj.each(function (_data) {
-      var svg = d3.select(this).select(".attr-poll > g")
+    var legend = '<div class="legend table">';
+    for(var i=1; i<dis.length;i++){
+      legend += '<div class="legend-element tr">'
+             + '<div class="td"><div class="legend-color" style="background-color:'+dis.colors[i]+'"></div></div>'
+             + '<div class="td legend-text">'+dis.ctext[i];
+      legend += '</div></div>';
+    }
+    legend += '</div>';
+    $('[data-pid='+dis.pid+']').append(legend);
+    // this.obj.each(function (_data) {
+    //   var svg = d3.select(this).select(".attr-poll > g")
 
-      var legend_xy = r*Math.sin(Math.PI/4); //quadrant
-      var legend_wh = r*2*Math.sin(Math.PI/4); //full h/w we can work with
-      var rect_h = legend_wh/dis.length;
-      var rect_w = 10;
-      var margin = 5;
-      var text_ybump = 0;
+    //   var legend_xy = r1*Math.sin(Math.PI/4); //quadrant
+    //   var legend_wh = r1*2*Math.sin(Math.PI/4); //full h/w we can work with
+    //   var y_disp = 2*r2-legend_xy;
+    //   var rect_h = legend_wh/dis.length;
+    //   var rect_w = 10;
+    //   var margin = 5;
+    //   var text_ybump = 0;
 
-      switch(dis.length){
-        case 2:
-          var font_size = 30;
-          break;
-        case 3:
-          var font_size = 24;
-          break;
-        case 4:
-          var font_size = 20;
-          break;
-        case 5:
-          var font_size = 18;
-          text_ybump = 1;
-          break;
-        case 6:
-          var font_size = 14;
-          text_ybump = 2;
-          break;
-      }
+    //   switch(dis.length){
+    //     case 2:
+    //       var font_size = 30;
+    //       break;
+    //     case 3:
+    //       var font_size = 24;
+    //       break;
+    //     case 4:
+    //       var font_size = 20;
+    //       break;
+    //     case 5:
+    //       var font_size = 18;
+    //       text_ybump = 1;
+    //       break;
+    //     case 6:
+    //       var font_size = 14;
+    //       text_ybump = 2;
+    //       break;
+    //   }
 
-      for(var i=0; i<dis.length;i++){
-        svg.append('rect')
-           .attr('x', (-legend_xy))
-           .attr('y', (-legend_xy+rect_h*i+margin/2))
-           .attr('width', rect_w)
-           .attr('height', rect_h-margin)
-           .attr('fill', dis.colors[i+1])
-        svg.append('text')
-           .attr('class', "legend_text")
-           .attr('width', rect_w)
-           .attr('x', (-legend_xy+rect_w*2)) // just right of color bar
-           .attr('y', (-legend_xy+rect_h*(i)+rect_h/2-margin/2+text_ybump)) // at the top of color bar
-           .attr("text-anchor", "start") //horizontal align to left of text
-           .attr('alignment-baseline', 'central') // vertical align to middle of text
-           .style("font-size", font_size+"px")
-           .text(dis.ctext[i]+' - 100%')
-      }
-    });
+    //   for(var i=0; i<dis.length;i++){
+    //     svg.append('rect')
+    //        .attr('x', (-2*legend_xy))
+    //        .attr('y', (-legend_xy+rect_h*i+margin/2 + y_disp))
+    //        .attr('width', rect_w)
+    //        .attr('height', rect_h-margin)
+    //        .attr('fill', dis.colors[i+1])
+    //     svg.append('text')
+    //        .attr('class', "legend_text")
+    //        .attr('width', rect_w)
+    //        .attr('x', (-legend_xy+rect_w*2)) // just right of color bar
+    //        .attr('y', (-legend_xy+rect_h*(i)+rect_h/2-margin/2+text_ybump)) // at the top of color bar
+    //        .attr("text-anchor", "start") //horizontal align to left of text
+    //        .attr('alignment-baseline', 'central') // vertical align to middle of text
+    //        .style("font-size", font_size+"px")
+    //        .text(dis.ctext[i]+' - 100%')
+    //   }
+    // });
   }
   
   //internal vars here
@@ -190,7 +203,7 @@ pie = (function (){
         tmp.push(0);
       }
       tmp.unshift(1);
-      $(_sel).append('<div class="pie-wrap" data-pid="'+_uid+'"><svg class="attr-poll"></svg></div>')
+      $(_sel).append('<div class="pie-wrap" data-pid="'+_uid+'" style="max-width:'+pieW+'px"><svg class="attr-poll"></svg></div>')
       return new Donut(d3.select('.pie-wrap[data-pid="'+_uid+'"]').datum(tmp), _uid);
     }
   }
