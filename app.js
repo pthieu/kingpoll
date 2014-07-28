50fb52fe046b2184cb3fcb44ddbd889bb5e4611e
Phong <pthieu@gmail.com>
2014-07-28 01:11:07 -0400

    hide kingpoll_attr polls from public

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ io.sockets.on('connection', function (client) {
         switch(sort){
             case 'newest':
             //TEMPORARY TO SEE RESULTS ON HOME
-                Poll.find({},{'p_id':1, 'p_q':1, 'p_total':1, 's_ttotal':1, 'p_desc':1, 'p_embed':1},{limit: limit, skip: skip}).sort('-_id').exec(function(err, poll) {
+                Poll.find({'u_id': {$not:/kingpoll_attr/i}},{'p_id':1, 'p_q':1, 'p_total':1, 's_ttotal':1, 'p_desc':1, 'p_embed':1},{limit: limit, skip: skip}).sort('-_id').exec(function(err, poll) {
                 // Poll.find({'u_id': {$not:/kingpoll_attr/i}},{'p_id':1, 'p_q':1, 'p_total':1, 's_ttotal':1, 'p_desc':1, 'p_embed':1},{limit: limit, skip: skip}).sort('-_id').exec(function(err, poll) {
                     if (err) return console.error(err);
                     client.emit('listpoll', poll);
