4616038f9928f6522e43aa52f0549ed3b7d3f265
Phong <pthieu@gmail.com>
2014-08-09 19:40:47 -0400

    if user goes to /u/, find req u_id and redirect so easier for user to copy url and give to people

--- routes/account.js
+++ routes/account.js
@@ -7,49 +7,51 @@ var Vote = mongoose.model( 'vote' );
 
 exports.getOwnAccount = function(req, res) {
   if (req.user) {
-    Vote.find({'u_id':req.user._id}, function (err, votes) {
+    // Vote.find({'u_id':req.user._id}, function (err, votes) {
+      // if (err) console.error(err);
+      var redirect = '/u/'+req.user.u_id.toString();
+      res.header('Content-Length', Buffer.byteLength(redirect));
+      res.redirect(redirect, 302);
+      // var longest = 0;
+      // var shortest = 999999;
+      // var average = 0;
+      // var total = 0;
+      // var votedisplay = new Array();
 
-      var longest = 0;
-      var shortest = 999999;
-      var average = 0;
-      var total = 0;
-      var votedisplay = new Array();
+      // if(votes.length > 0) {
+      //   votes.forEach( function (vote){
+      //     total = total + vote.s_vtime;
+      //     if(longest < vote.s_vtime)
+      //       longest = vote.s_vtime;
+      //     if(shortest > vote.s_vtime)
+      //       shortest = vote.s_vtime;
+      //   });
 
-      if(votes.length > 0) {
-        votes.forEach( function (vote){
-          total = total + vote.s_vtime;
-          if(longest < vote.s_vtime)
-            longest = vote.s_vtime;
-          if(shortest > vote.s_vtime)
-            shortest = vote.s_vtime;
-        });
+      //   average = total / votes.length;
+      // } else {
+      //   shortest = 0;
+      // }
 
-        average = total / votes.length;
-      } else {
-        shortest = 0;
-      }
+      // votedisplay['longest'] = (longest/1000).toFixed(2);
+      // votedisplay['shortest'] = (shortest/1000).toFixed(2);
+      // votedisplay['average'] = (average/1000).toFixed(2);
+      // votedisplay['total'] = (total/1000).toFixed(2);
 
-      votedisplay['longest'] = (longest/1000).toFixed(2);
-      votedisplay['shortest'] = (shortest/1000).toFixed(2);
-      votedisplay['average'] = (average/1000).toFixed(2);
-      votedisplay['total'] = (total/1000).toFixed(2);
-
-      Poll.find({'u_id':req.user._id}, function (err, polls) {
-        res.render('account', 
-          { title: req.user.u_id + "'s Info", 
-            user: req.user, 
-            showbuttons: false,
-            polls: polls, 
-            pollslength: polls.length,
-            voteslength: votes.length,
-            votedisplay: votedisplay, 
-            js_script:'/js/account.js' });
-      });
-    });
+      // Poll.find({'u_id':req.user._id}, function (err, polls) {
+      //   res.render('account', 
+      //     { title: req.user.u_id + "'s Info", 
+      //       user: req.user, 
+      //       showbuttons: false,
+      //       polls: polls, 
+      //       pollslength: polls.length,
+      //       voteslength: votes.length,
+      //       votedisplay: votedisplay, 
+      //       js_script:'/js/account.js' });
+      // });
+    // });
   } else {
     res.render('login', { error: true, title: "Kingpoll Login" });
   }
-  
 };
 
 exports.getUserAccount = function(req, res) {
@@ -60,33 +62,33 @@ exports.getUserAccount = function(req, res) {
     if(user){
       Vote.find({'u_id':user._id}, function (err, votes) {
 
-      var longest = 0;
-      var shortest = 999999;
-      var average = 0;
-      var total = 0;
-      var votedisplay = new Array();
-      if(votes.length > 0) {
-        votes.forEach( function (vote){
-          total = total + vote.s_vtime;
-          if(longest < vote.s_vtime)
-            longest = vote.s_vtime;
-          if(shortest > vote.s_vtime)
-            shortest = vote.s_vtime;
-        });
+        var longest = 0;
+        var shortest = 999999;
+        var average = 0;
+        var total = 0;
+        var votedisplay = new Array();
+        if(votes.length > 0) {
+          votes.forEach( function (vote){
+            total = total + vote.s_vtime;
+            if(longest < vote.s_vtime)
+              longest = vote.s_vtime;
+            if(shortest > vote.s_vtime)
+              shortest = vote.s_vtime;
+          });
 
-        average = total / votes.length;
-      } else {
-        shortest = 0;
-      }
+          average = total / votes.length;
+        } else {
+          shortest = 0;
+        }
 
-      votedisplay['longest'] = (longest/1000).toFixed(2);
-      votedisplay['shortest'] = (shortest/1000).toFixed(2);
-      votedisplay['average'] = (average/1000).toFixed(2);
-      votedisplay['total'] = (total/1000).toFixed(2);
+        votedisplay['longest'] = (longest/1000).toFixed(2);
+        votedisplay['shortest'] = (shortest/1000).toFixed(2);
+        votedisplay['average'] = (average/1000).toFixed(2);
+        votedisplay['total'] = (total/1000).toFixed(2);
 
-      Poll.find({'u_id':user._id}, function (err, polls) {
-        res.render('account', 
-          { title: user.u_id + "'s Info", 
+        Poll.find({'u_id':user._id}, function (err, polls) {
+          res.render('account', 
+            { title: user.u_id + "'s Info", 
             user: user, 
             showbuttons: false,
             createUpl: true,
@@ -95,10 +97,10 @@ exports.getUserAccount = function(req, res) {
             voteslength: votes.length,
             votedisplay: votedisplay, 
             js_script:'/js/account.js' });
+        });
       });
-    });
-    } else {
-      res.render('account', { title: "This user does not exist!"});
-    }     
-  });
+} else {
+  res.render('account', { title: "This user does not exist!"});
+}     
+});
 };
