290762a9f94abb40e504498ba0b9a763c86dfc7f
Phong <pthieu@gmail.com>
2014-08-11 20:01:30 -0400

    color on login button

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-    $("input.f").on("focusout", function(){
-      var me = $(this);
-      if(me.val() !== ""){
-        me.addClass("set");
-      } else {
-        me.removeClass("set");
-      }
-    });
+  $("input.f").on("focusout", function(){
+    var me = $(this);
+    if(me.val() !== ""){
+      me.addClass("set");
+    } else {
+      me.removeClass("set");
+    }
+  });
+
+  var themecolor = colors_hex[randColor(colors_hex)];
+  $('#submit').css({'background-color': "#"+themecolor,'border-color': "#"+themecolor});
+  $('.field').hover(function () {
+    $(this).find('label').css({'color': "#"+themecolor});
+    $(this).find('input, label').css({'color': "#"+themecolor});
+  }, function () {
+    $(this).find('label').css({'color': "#888"});
+    $(this).find('input, label').css({'color': "#888"});
+  });
 });
